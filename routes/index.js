f8510eab5d3220a906ccb4963bc3f98d68e237ed
Chan, Danny <danny50610@yahoo.com.tw>
2016-06-01 23:14:04 +0800

    改寫成jade && 變更網站名稱

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'FCU Simple Smart Home' });
 });
 
 module.exports = router;

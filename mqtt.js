7d2de6c58918ced2245f98a56293b50498fc76a3
Chan, Danny <danny50610@yahoo.com.tw>
2016-06-11 19:15:05 +0800

    整理出函式

--- mqtt.js
+++ mqtt.js
@@ -3,6 +3,25 @@ var moment = require('moment');
 var mqtt = require('mqtt');
 var client = undefined;
 
+function getNowSqlTimestamp() {
+    return moment().format('YYYY-MM-DD HH:mm:ss');
+}
+
+function saveTemperature(val) {
+    if (!Number.isInteger(val)) {
+        console.log("val is not a number");
+        return false;
+    }
+
+    mysql.pool.query('INSERT INTO temperature SET ?', {value: val, saved: getNowSqlTimestamp()}, function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    return true;
+}
+
 exports.initMqttClient =
     function initMqttClient(host) {
         client = mqtt.connect('mqtt://' + host);
@@ -13,16 +32,12 @@ exports.initMqttClient =
             client.subscribe("sensor_temperature");
         });
 
-        client.on('message', function(topic, message, packet) {
+        client.on('message', function (topic, message, packet) {
             var msg = message.toString();
             console.log(topic, msg);
 
             if (topic == 'sensor_temperature') {
-                mysql.pool.query('INSERT INTO temperature SET ?', {value: msg, saved: moment().format('YYYY-MM-DD HH:mm:ss')}, function(err, result) {
-                    if (err) throw err;
-
-                    console.log(result.insertId);
-                });
+                saveTemperature(parseInt(msg));
             }
         });
 

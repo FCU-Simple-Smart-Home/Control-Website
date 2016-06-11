c060c76bb66028aec6eb5f9232a40807c3262f64
Chan, Danny <danny50610@yahoo.com.tw>
2016-06-11 23:49:57 +0800

    Reformat code

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function (req, res, next) {
 router.get('/normal-monitor', function (req, res, next) {
     normalMonitorController.index(function (data) {
         res.render('normal-monitor', merge({
-            title: '一般偵測 - FCU Simple Smart Home',
+            title: '一般偵測 - FCU Simple Smart Home'
         }, data));
     })
 });

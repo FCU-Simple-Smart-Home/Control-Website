8060b2a96508c8a6cee3a1bcfc2139ab326869d1
Chan, Danny <danny50610@yahoo.com.tw>
2016-06-12 17:16:13 +0800

    Show gas, co chart

--- controller/NormalMonitorController.js
+++ controller/NormalMonitorController.js
@@ -2,13 +2,21 @@ var merge = require('merge');
 
 var temperature = require('../model/temperature.js');
 var humidity = require('../model/humidity.js');
+var gas = require('../model/gas.js');
+var co = require('../model/co.js');
 
 exports.index = function index(callback) {
     // FIXME: callback hell
     temperature.get5MinData(function (result) {
         var data = {data_temperature: handleResult('temperature', result)};
         humidity.get5MinData(function (result) {
-            callback(merge(data, {data_humidity: handleResult('humidity', result)}));
+            data = merge(data, {data_humidity: handleResult('humidity', result)});
+            gas.get5MinData(function (result) {
+                data = merge(data, {data_gas: handleResult('gas', result)});
+                co.get5MinData(function (result) {
+                    callback(merge(data, {data_co: handleResult('co', result)}));
+                });
+            });
         });
     });
 };

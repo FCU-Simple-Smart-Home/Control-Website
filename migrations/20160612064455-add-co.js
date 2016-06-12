b88f800e67b3910de8804aeff3c76314aed8c789
Chan, Danny <danny50610@yahoo.com.tw>
2016-06-12 14:47:07 +0800

    Add gas & co table

---
+++ migrations/20160612064455-add-co.js
@@ -0,0 +1,14 @@
+var dbm = global.dbm || require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function (db, callback) {
+  db.createTable('co', {
+    id: {type: 'int', primaryKey: true, autoIncrement: true},
+    value: {type: 'int', notNull:true},
+    saved: {type: 'timestamp', notNull:true, unique: true}
+  }, callback);
+};
+
+exports.down = function (db, callback) {
+  db.dropTable('humidity', callback);
+};
